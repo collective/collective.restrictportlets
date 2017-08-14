973ef2aeea7be0b6dcedfe7dbc72c6fa2a4b7d19
Maurits van Rees <maurits@vanrees.org>
2017-08-14 14:43:37 +0200

    Back to development: 1.0a2
    [ci skip]

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.restrictportlets',
-    version='1.0a1',
+    version='1.0a2.dev0',
     description="Restrict addable portlet types",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
