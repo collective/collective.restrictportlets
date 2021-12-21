a4fca78f699471ea9f00c042de553033e03a2d03
Maurits van Rees <maurits@vanrees.org>
2021-12-21 15:41:56 +0100

    Fixed installer code in test_setup.py to also work in Plone 5.2+.

--- src/collective/restrictportlets/tests/test_setup.py
+++ src/collective/restrictportlets/tests/test_setup.py
@@ -12,6 +12,15 @@ from zope.component import getUtility
 import unittest
 
 
+try:
+    # Plone 5.1+
+    from Products.CMFPlone.utils import get_installer
+except ImportError:
+    # Plone 5.0/4.3
+    def get_installer(context, request=None):
+        return api.portal.get_tool("portal_quickinstaller")
+
+
 class TestSetup(unittest.TestCase):
     """Test that collective.restrictportlets is properly installed."""
 
@@ -20,13 +29,17 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer["portal"]
-        self.installer = api.portal.get_tool("portal_quickinstaller")
+        self.installer = get_installer(self.portal)
 
     def test_product_installed(self):
         """Test if collective.restrictportlets is installed."""
-        self.assertTrue(
-            self.installer.isProductInstalled("collective.restrictportlets")
-        )
+        if hasattr(self.installer, "is_product_installed"):
+            installed = self.installer.is_product_installed(
+                "collective.restrictportlets"
+            )
+        else:
+            installed = self.installer.isProductInstalled("collective.restrictportlets")
+        self.assertTrue(installed)
 
     def test_browserlayer(self):
         """Test that ICollectiveRestrictportletsLayer is registered."""
@@ -44,14 +57,21 @@ class TestUninstall(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer["portal"]
-        self.installer = api.portal.get_tool("portal_quickinstaller")
-        self.installer.uninstallProducts(["collective.restrictportlets"])
+        self.installer = get_installer(self.portal)
+        if hasattr(self.installer, "uninstall_product"):
+            self.installer.uninstall_product("collective.restrictportlets")
+        else:
+            self.installer.uninstallProducts(["collective.restrictportlets"])
 
     def test_product_uninstalled(self):
         """Test if collective.restrictportlets is cleanly uninstalled."""
-        self.assertFalse(
-            self.installer.isProductInstalled("collective.restrictportlets")
-        )
+        if hasattr(self.installer, "is_product_installed"):
+            installed = self.installer.is_product_installed(
+                "collective.restrictportlets"
+            )
+        else:
+            installed = self.installer.isProductInstalled("collective.restrictportlets")
+        self.assertFalse(installed)
 
     def test_browserlayer_removed(self):
         """Test that ICollectiveRestrictportletsLayer is removed."""
